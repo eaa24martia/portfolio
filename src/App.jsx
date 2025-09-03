f2d54ba541cc47cecff61c9f798ad587d76ad509
eaa24martia <eaa24martia@gmail.com>
2025-09-03 10:17:43 +0200

    start with deploying app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/eaa24martia/"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+import Navigation from "../components/Navigation";
+
+export default function Home() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Home</h1>
+    </div>
+  );
+}
