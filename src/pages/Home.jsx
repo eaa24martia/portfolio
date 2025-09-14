91f4aaea3b4e56a433c0fa08dfee6168eda62d7e
eaa24martia <eaa24martia@gmail.com>
2025-09-14 18:13:18 +0200

    added colour palette

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Section from "../components/Section";
 import Slider from "../components/Slider";
+import Competencies from "../components/Competencies";
 
 export default function Home() {
   return (
@@ -11,17 +12,15 @@ export default function Home() {
   image="./public/img/logo.png"
   className="home-hero"
 />
-  <Slider />
-
-    <Section
+  <Slider /> 
+  <Competencies />
+   <Section
   subtitle="Hej, I'm Martin"
   title="Multimedia Designer"
   tagline="A danish Multimedia Design student passionate about improving outdated digital experiences and building new ones. Specialized in UX and visual design, while sharpening my frontend programming skills."
   image="./public/img/logo.png"
   className="home-hero"
 />
-  
-  
 </main>
 
 
