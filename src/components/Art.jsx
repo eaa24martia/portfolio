e1f5a9f9133ace33ed22efa14e7fb2a9bd693d05
eaa24martia <eaa24martia@gmail.com>
2025-09-16 14:04:39 +0200

    finished set up of about me page

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -2,24 +2,40 @@
 export default function Art () {
   
   return (
-    <section className="art-section-container">
+<section className="art-section-container">
         <h2>My digital art</h2>
+
+        <section className="art-sections">
+
     
-    <section className="art-section">
+   <section className="art-section">
         <section>
-            <img src="https://placehold.co/600x400/4b694e/faf3de?text=Placeholder+Image" alt="Digital Art 1" />
-        </section>
-        <section>
-            <img src="https://placehold.co/600x400/4b694e/faf3de?text=Placeholder+Image" alt="Digital Art 2" />
-        </section>
-        <section>
-            <img src="https://placehold.co/600x400/4b694e/faf3de?text=Placeholder+Image" alt="Digital Art 3" />
-        </section>
-        <section>
-            <img src="https://placehold.co/600x400/4b694e/faf3de?text=Placeholder+Image" alt="Digital Art 4" />
+            <img src="/img/blub.png" alt="Digital Art 1" />
         </section>
     </section>
 
+     <section className="art-section2">
+        <section>
+            <img src="/img/jinx-watercolor.png" alt="Digital Art 1" />
+        </section>
     </section>
+
+     <section className="art-section3">
+        <section>
+            <img src="/img/lifeweaver.png" alt="Digital Art 1" />
+        </section>
+    </section>
+
+     <section className="art-section4">
+        <section>
+            <img src="/img/avatar.png" alt="Digital Art 1" />
+        </section>
+    </section>
+
+        </section>
+
+</section>
+
+  
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,14 @@
 
 export default function Contact() {
   return (
-   <h1>Contact</h1>
+
+    <section className="contact-page-container">
+      <h1>Let's talk</h1>
+
+      <h2>tvebakmartin@gmail.com</h2>
+      <h2>+45 60 69 25 05</h2>
+
+      <img src="" alt="" />
+    </section>
   );
 }
