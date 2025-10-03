7891efd02f51fb7fa62958030aadbcc0cc58e158
eaa24martia <eaa24martia@gmail.com>
2025-10-03 17:06:59 +0200

    changed slider, so it works for mobile

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -100,6 +100,27 @@ export default function SwiperComponent() {
               modifier: 2.5,
               slideShadows: false,
             }}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                effect: 'slide',
+                centeredSlides: true,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: -20,
+                effect: 'coverflow',
+                centeredSlides: true,
+                coverflowEffect: { 
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 50,
+                  modifier: 2.5,
+                  slideShadows: false,
+                }
+              }
+            }}
             pagination={{ clickable: true }}
             navigation={false}
             modules={[EffectCoverflow, Pagination, Navigation, Mousewheel]}
